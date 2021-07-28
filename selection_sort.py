f602c8dcc746c92ee6303a9ddfe286b547337e80
ShraddhaMD <shraddhamd08@gmail.com>
2021-07-28 14:47:44 +0530

    selection

---
+++ selection_sort.py
@@ -0,0 +1,7 @@
+li = []
+li = list(map(int,input().split()))
+for i in range (len(li)):
+    for j in range(i+1,len(li)):
+        if li[i] > li[j]:
+            li[i] ,li[j]= li[j],li[i]
+print(li)
